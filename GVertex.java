ef4271bcf6403e471ee6d990b2416132a1f8860e
Jon Karyo <jkaryo1@jhu.edu>
2016-04-22 15:04:47 -0400

    GVertex

--- GVertex.java
+++ GVertex.java
@@ -1,7 +1,13 @@
 /**
  * Implementation of a generic vertex class.
+ * @author Jon Karyo, Calvin Knowlton, David Feldman, Derek Fischer
+ * jkaryo1, cknowlt3, dfeldma9, dfisch11
+ * P4
+ * 600.226
  */
-public class GVertex<T extends Comparable<? super T>> extends Vertex {
+public class GVertex<T extends Comparable<? super T>> 
+        implements Comparable<GVertex> {
+
     /** Vertex unique ID number. */
     private int num;
 
