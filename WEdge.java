9f0cb97456bdbbf966e1e19b0ab63794056ac284
Jon Karyo <jkaryo1@jhu.edu>
2016-04-22 14:32:43 -0400

    Added names

--- WEdge.java
+++ WEdge.java
@@ -1,5 +1,9 @@
 /**
  * Implementation of a weighted edge class.
+ * @author Jon Karyo, Calvin Knowlton, David Feldman, Derek Fischer
+ * jkaryo1, cknowlt3, dfeldma9, dfisch11
+ * P4
+ * 600.226
  */
 public class WEdge extends Edge implements Comparable<WEdge> {
     
