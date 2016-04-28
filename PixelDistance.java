2a96dc00af777488e936a7900e2833bd26004260
Jon Karyo <jkaryo1@jhu.edu>
2016-04-28 18:39:15 -0400

    Checkstyle PixelDistance

--- PixelDistance.java
+++ PixelDistance.java
@@ -7,21 +7,30 @@
  */
 public class PixelDistance implements Distance<Pixel> {
 
+    /**Value for a full byte.*/
+    public static final int FULL = 0xFF;
+    /**For second byte taken out.*/
+    public static final int SECOND = 8;
+    /**For third byte taken out.*/
+    public static final int THIRD = 16;
+    /**For fourth byte taken out.*/
+    public static final int FOURTH = 24;
+    
     @Override
     public double distance(Pixel one, Pixel two) {
-        int b1 = one.value() & 0xFF;
-        int g1 = (one.value() >> 8) & 0xFF;
-        int r1 = (one.value() >> 16) & 0xFF;
-        int e1 = (one.value() >> 24) & 0xFF;
-        int b2 = two.value() & 0xFF;
-        int g2 = (two.value() >> 8) & 0xFF;
-        int r2 = (two.value() >> 16) & 0xFF;
-        int e2 = (two.value() >> 24) & 0xFF;
+        int b1 = one.value() & FULL;
+        int g1 = (one.value() >> SECOND) & FULL;
+        int r1 = (one.value() >> THIRD) & FULL;
+        int e1 = (one.value() >> FOURTH) & FULL;
+        int b2 = two.value() & FULL;
+        int g2 = (two.value() >> SECOND) & FULL;
+        int r2 = (two.value() >> THIRD) & FULL;
+        int e2 = (two.value() >> FOURTH) & FULL;
 
-        int b = (int) Math.pow((b1-b2), 2);
-        int g = (int) Math.pow((g1-g2), 2);
-        int r = (int) Math.pow((r1-r2), 2);
-        int e = (int) Math.pow((e1-e2), 2);
+        int b = (int) Math.pow((b1 - b2), 2);
+        int g = (int) Math.pow((g1 - g2), 2);
+        int r = (int) Math.pow((r1 - r2), 2);
+        int e = (int) Math.pow((e1 - e2), 2);
 
         return b + g + r + e;
     }
