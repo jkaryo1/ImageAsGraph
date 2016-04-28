005cd6446f839da43dff551631713efbefe9da46
DavidFeldman <davidelanfeldman@gmail.com>
2016-04-28 18:33:36 -0400

    Updated to be checkstyle compliant.

--- PixelDistance.java
+++ PixelDistance.java
@@ -1,19 +1,29 @@
 /**
- * PixelDistance class implementation of Distance.
+ * * PixelDistance class implementation of Distance.
  * @author Jon Karyo, Calvin Knowlton, David Feldman, Derek Fischer
  * jkaryo1, cknowlt3, dfeldma9, dfisch11
  * P4
  * 600.226
- * @param <Pixel> the abstract data type.
  */
-public class PixelDistance<Pixel> implements Distance<Pixel> {
+public class PixelDistance implements Distance<Pixel> {
 
     @Override
     public double distance(Pixel one, Pixel two) {
         int b1 = one.value() & 0xFF;
-        int g1 = ((int)one >> 8) & 0xFF;
-        int r1 = ((int) one >> 16) & 0xFF;
-        return 0;
+        int g1 = (one.value() >> 8) & 0xFF;
+        int r1 = (one.value() >> 16) & 0xFF;
+        int e1 = (one.value() >> 24) & 0xFF;
+        int b2 = two.value() & 0xFF;
+        int g2 = (two.value() >> 8) & 0xFF;
+        int r2 = (two.value() >> 16) & 0xFF;
+        int e2 = (two.value() >> 24) & 0xFF;
+
+        int b = (int) Math.pow((b1-b2), 2);
+        int g = (int) Math.pow((g1-g2), 2);
+        int r = (int) Math.pow((r1-r2), 2);
+        int e = (int) Math.pow((e1-e2), 2);
+
+        return b + g + r + e;
     }
     
 }
